b937745f761b2c86a1dae56a8d899660b7746b9d
qwwqe <f.werth@gmail.com>
2024-12-07 17:27:08 +0800

    Define ManufacturerTile specs for standard game

--- specs/standard/mat.go
+++ specs/standard/mat.go
@@ -31,6 +31,20 @@ var playerMatSpec = blic.PlayerMatSpec{
 		breweryTileSpecLevel3,
 		breweryTileSpecLevel4,
 	},
+
+	ManufacturerTiles: []blic.IndustryTileSpec{
+		manufacturerTileSpecLevel1,
+		manufacturerTileSpecLevel2,
+		manufacturerTileSpecLevel2,
+		manufacturerTileSpecLevel3,
+		manufacturerTileSpecLevel4,
+		manufacturerTileSpecLevel5,
+		manufacturerTileSpecLevel5,
+		manufacturerTileSpecLevel6,
+		manufacturerTileSpecLevel7,
+		manufacturerTileSpecLevel8,
+		manufacturerTileSpecLevel8,
+	},
 }
 
 var coalMineTileSpecLevel1 = blic.IndustryTileSpec{
@@ -294,5 +308,181 @@ var breweryTileSpecLevel4 = blic.IndustryTileSpec{
 		CostInPounds: 9,
 		CostInCoal:   0,
 		CostInIron:   1,
+	},
+}
+
+var manufacturerTileSpecLevel1 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    1,
+		NumLinks: 2,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      3,
+		IncomeBoost:        5,
+		BeerRequiredToSell: 1,
+
+		RequiredEra: address(blic.EraCanal),
+		CanDevelop:  true,
+
+		CostInPounds: 8,
+		CostInCoal:   1,
+		CostInIron:   0,
+	},
+}
+
+var manufacturerTileSpecLevel2 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    2,
+		NumLinks: 1,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      5,
+		IncomeBoost:        1,
+		BeerRequiredToSell: 1,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 10,
+		CostInCoal:   0,
+		CostInIron:   1,
+	},
+}
+
+var manufacturerTileSpecLevel3 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    3,
+		NumLinks: 0,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      4,
+		IncomeBoost:        4,
+		BeerRequiredToSell: 0,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 12,
+		CostInCoal:   2,
+		CostInIron:   0,
+	},
+}
+
+var manufacturerTileSpecLevel4 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    4,
+		NumLinks: 1,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      3,
+		IncomeBoost:        6,
+		BeerRequiredToSell: 1,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 8,
+		CostInCoal:   0,
+		CostInIron:   1,
+	},
+}
+
+var manufacturerTileSpecLevel5 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    5,
+		NumLinks: 2,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      8,
+		IncomeBoost:        2,
+		BeerRequiredToSell: 2,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 16,
+		CostInCoal:   1,
+		CostInIron:   0,
+	},
+}
+
+var manufacturerTileSpecLevel6 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    6,
+		NumLinks: 1,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      7,
+		IncomeBoost:        6,
+		BeerRequiredToSell: 1,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 20,
+		CostInCoal:   0,
+		CostInIron:   0,
+	},
+}
+
+var manufacturerTileSpecLevel7 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    7,
+		NumLinks: 0,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      9,
+		IncomeBoost:        4,
+		BeerRequiredToSell: 0,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 16,
+		CostInCoal:   1,
+		CostInIron:   1,
+	},
+}
+
+var manufacturerTileSpecLevel8 = blic.IndustryTileSpec{
+	IndustryTile: blic.IndustryTile{
+		Type:     blic.IndustryTypeManufacturer,
+		Level:    8,
+		NumLinks: 1,
+
+		CanalEraResources: 0,
+		RailEraResources:  0,
+
+		VictoryPoints:      11,
+		IncomeBoost:        1,
+		BeerRequiredToSell: 1,
+
+		RequiredEra: nil,
+		CanDevelop:  true,
+
+		CostInPounds: 20,
+		CostInCoal:   0,
+		CostInIron:   2,
 	},
 }
